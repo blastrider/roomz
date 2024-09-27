a44237965e31a9a59897ec4227dc33bf418d024a
blastrider <guimaxali@gmail.com>
2024-09-27 22:50:34 +0200

    les lgos ça doit être dans le main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{App, HttpServer};
+use flexi_logger::{Duplicate, FileSpec, FlexiLoggerError, Logger, WriteMode};
 use handlers::{reservations_config, rooms_config, users_config};
 
 mod db;
@@ -9,6 +10,31 @@ mod schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Configure flexi_logger
+    // Configure flexi_logger
+    let log_file_name = format!("roomz_{}.log", chrono::Local::now().format("%Y-%m-%d"));
+    Logger::try_with_str("info")
+        .map_err(|e: FlexiLoggerError| {
+            std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
+        })?
+        .log_to_file(FileSpec::default().basename(log_file_name))
+        .duplicate_to_stdout(Duplicate::Info)
+        .write_mode(WriteMode::BufferAndFlush)
+        .append()
+        .format_for_files(flexi_logger::detailed_format)
+        .use_utc()
+        .rotate(
+            flexi_logger::Criterion::Size(10_000_000), // Rotate when file size exceeds 10 MB
+            flexi_logger::Naming::Numbers,
+            flexi_logger::Cleanup::KeepLogFiles(7), // Keep the last 7 log files
+        )
+        .start()
+        .map_err(|e: FlexiLoggerError| {
+            std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
+        })?;
+
+    // Application initialization
+
     db::init();
 
     HttpServer::new(|| {
